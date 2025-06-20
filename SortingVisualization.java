90d6cf59b563e9fd63b2e258d876865a345e745c
viio00 <194786916+viio00@users.noreply.github.com>
2025-06-20 18:05:37 +0800

    might add other sorting algorithms later

--- SortingVisualization.java
+++ SortingVisualization.java
@@ -3,7 +3,6 @@ package application;
 //first time using javaFx so I have GPT as my guide
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.stage.Stage; //main window
 import javafx.scene.Scene; //holds all visual content
@@ -18,15 +17,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
-
 public class SortingVisualization extends Application {
 
 	Stage window; //stores the stage to switch pages/scenes
-	Scene scene1, scene2; 
+	Scene scene1, scene2, scene3, scene4;
 	
-	static final int Num_bars = 60; //60 rectangle bars
-	static final int Max_height = 300; //random heights no more than 300px
-	Rectangle[] bars = new Rectangle[Num_bars]; //array that stores 60 bars
+	public static final int Num_bars = 60; //60 rectangle bars
+	public static final int Max_height = 300; //random heights no more than 300px
+	public static Rectangle[] bars = new Rectangle[Num_bars]; //array that stores 60 bars
 	
 	@Override //main GUI
 	public void start(Stage primaryStage){
@@ -37,8 +35,8 @@ public class SortingVisualization extends Application {
 		Button quickSort = new Button("Quick Sort");
 		
 		bubbleSort.setOnAction(e -> showBubbleSort("Bubble Sort"));
-        //mergeSort.setOnAction(e -> showMergeSort("Merge Sort"));
-        //quickSort.setOnAction(e -> showQuickSort("Quick Sort"));
+        mergeSort.setOnAction(e -> showMergeSort("Merge Sort"));
+        quickSort.setOnAction(e -> showQuickSort("Quick Sort"));
 		
         VBox buttons = new VBox(4); //spacing 4px (gap between buttons)
         buttons.getChildren().addAll(bubbleSort, mergeSort, quickSort);
@@ -72,35 +70,133 @@ public class SortingVisualization extends Application {
 			//getChildren() -- gives access to the HBox
 		}
 		
-		VBox layout2 = new VBox(20);
-		layout2.getChildren().addAll(func, box);
-		layout2.setAlignment(Pos.CENTER);
-		scene2 = new Scene(layout2, 750, 400);
+		VBox layout = new VBox(50);
+		layout.getChildren().addAll(func, box);
+		layout.setAlignment(Pos.CENTER);
+		scene2 = new Scene(layout, 750, 420);
 		window.setScene(scene2);
 		
 		
 		/*	first time using threads although I am familiar with it.
 		 	basically, thread allows a program to do multiple things
 		 	at once */
-		new Thread(() -> bubbleSort()).start();
+		bubbleSortClass bubble = new bubbleSortClass(bars);
+		new Thread(() -> {
+			try {
+				bubble.bubbleSort();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}).start();
 	}
 	
-	public void bubbleSort() {
-		try {
-			for (int i = 0; i < bars.length-1; i++) {
-				for (int j = 0; j < bars.length-1; j++) {
-					if(bars[j].getHeight() > bars[j+1].getHeight()) {
-						swap(j, j+1);
-						Thread.sleep(20);
-						}
-					}
+	public void showMergeSort(String type) {
+		int[] array = new int[Num_bars];
+		Text sortType = new Text(type);
 		
-				}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		VBox func = new VBox(4);
+		func.setAlignment(Pos.CENTER);
+		Button back = new Button("back");
+		back.setOnAction(e -> window.setScene(scene1));
+		func.getChildren().addAll(sortType, back);
+		
+		HBox box = new HBox(2);
+		box.setAlignment(Pos.BASELINE_CENTER);
+
+		
+		for (int i = 0; i < Num_bars; i++) {
+			int height = (int)(Math.random() * Max_height);
+			array[i] = height;
+			
+			bars[i] = new Rectangle(10, height, Color.BLACK);
+			box.getChildren().add(bars[i]);
+		}
+		
+		VBox layout = new VBox(50);
+		layout.getChildren().addAll(func, box);
+		layout.setAlignment(Pos.CENTER);
+		scene3 = new Scene(layout, 750, 420);
+		window.setScene(scene3);
+		
+		mergeSortClass merge = new mergeSortClass(bars);
+		new Thread(() -> {
+			try {
+				merge.mergeSort(array);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
+		}).start();
+		
 	}
-	public void swap(int i, int j) {
+	
+	public void showQuickSort(String type) {
+		int[] array = new int[Num_bars];
+		Text sortType = new Text(type);
+		
+		VBox func = new VBox(4);
+		func.setAlignment(Pos.CENTER);
+		Button back = new Button("back");
+		back.setOnAction(e -> window.setScene(scene1));
+		func.getChildren().addAll(sortType, back);
+		
+		HBox box = new HBox(2);
+		box.setAlignment(Pos.BASELINE_CENTER);
+
+		
+		for (int i = 0; i < Num_bars; i++) {
+			int height = (int)(Math.random() * Max_height);
+			array[i] = height;
+			
+			bars[i] = new Rectangle(10, height, Color.BLACK);
+			box.getChildren().add(bars[i]);
+		}
+		
+		VBox layout = new VBox(50);
+		layout.getChildren().addAll(func, box);
+		layout.setAlignment(Pos.CENTER);
+		scene4 = new Scene(layout, 750, 420);
+		window.setScene(scene4);
+		
+		quickSortClass quick = new quickSortClass(bars);
+		new Thread(() -> {
+			try {
+				quick.quickSort(array, 0, array.length - 1);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}).start();
+		
+	}
+	
+public static void main(String[] args) {
+	launch(args);
+		
+	}
+}
+
+//******************** Sorting Algorithms ********************
+
+class bubbleSortClass {
+	private Rectangle[] bars;
+	
+	public bubbleSortClass(Rectangle[] bars) {
+		this.bars = bars;
+	}
+	
+	public void bubbleSort() throws InterruptedException{
+		for (int i = 0; i < bars.length-1; i++) {
+			for (int j = 0; j < bars.length-1; j++) {
+				if(bars[j].getHeight() > bars[j+1].getHeight()) {
+					swap(j, j+1);
+					Thread.sleep(20);
+				}
+			}
+		}
+	}
+	private void swap(int i, int j) {
 		double h1 = bars[i].getHeight();
 		double h2 = bars[j].getHeight();
 	
@@ -108,19 +204,116 @@ public class SortingVisualization extends Application {
 			bars[i].setHeight(h2);
 			bars[j].setHeight(h1);
 		});
+
+		/*	Platform.runLater(() -> {}); -- since we are running a code background,
+		 	it safely updates GUI. Basically waiting for thread to be ready
+		 	in order for us to see the bars swap */
+	}
+}
+class mergeSortClass {
+	private Rectangle[] bars;
 	
-	/*	Platform.runLater(() -> {}); -- since we are ruuning a code background,
-	 	it safely updates GUI. Basically waiting for thread to be ready
-	 	in order for us to see the bars swap */
+	 public mergeSortClass(Rectangle[] bars) {
+	        this.bars = bars;
+	  }
+	
+	 private void merge(int arr[], int l, int m, int r) throws InterruptedException {
+		int n1 = m - l + 1, n2 = r - m;
+		int L[] = new int [n1], R[] = new int[n2];
+		
+		for (int i = 0; i < n1; i++) L[i] = arr[l+i];
+		for (int j = 0; j < n2; j++) R[j] = arr [m + 1 + j];
+		
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i++];
+				updateBar(k, arr[k]);
+				Thread.sleep(20);
+			} else {
+				arr[k] = R[j++];
+				updateBar(k, arr[k]);
+				Thread.sleep(20);
+			}
+			updateBar(k, arr[k]);
+			k++;
+		}
+		
+		while (i < n1) {
+			arr[k] = L[i++];
+			updateBar(k, arr[k]);
+			Thread.sleep(20);
+			k++;
+		}
+		while (j < n2) {
+			arr[k] = R[j++];
+			updateBar(k, arr[k]);
+			Thread.sleep(20);
+			k++;
+		}	
+	}
+	public void mergeSort(int arr[]) throws InterruptedException {
+		for (int size = 1; size < arr.length; size *= 2) {
+			for (int left = 0; left < arr.length - size; left += 2*size) {
+				int mid = left + size - 1;
+				int right = Math.min(left + 2*size-1, arr.length - 1);
+				merge(arr, left, mid, right);
+			}
+		}
+	
+	}
+	 private void updateBar(int i, int h) {
+		javafx.application.Platform.runLater(() -> {
+			bars[i].setHeight(h);
+		});
+	}
 }
 
-	public void mergeSort(){
+class quickSortClass{
+	private Rectangle[] bars;
 	
+	public quickSortClass(Rectangle[] bars) {
+		this.bars = bars;
 	}
 	
-	
-public static void main(String[] args) {
-	launch(args);
+	 private int partition(int[] arr, int low, int high)throws InterruptedException {
+		int pivot = arr[high];
+		int i = low - 1;
 		
+		for (int j = low; j <= high - 1; j++) {
+			if(arr[j] < pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i+1, high);
+		return i + 1;
 	}
+	
+	private void swap(int[] arr, int i, int j) throws InterruptedException {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		
+		updateBar(i, arr[i]);
+		updateBar(j, arr[j]);
+        Thread.sleep(20);
+	}
+	
+	public void quickSort(int[] arr, int low, int high) throws InterruptedException {
+		if(low < high) {
+			//partition, index of pivot
+			int pi = partition(arr, low, high);
+			
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+	}
+	
+	private void updateBar(int i, int h) {
+		javafx.application.Platform.runLater(() -> {
+			bars[i].setHeight(h);
+		});
+	}
+	
 }
